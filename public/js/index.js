946059b6b77b74d8a73b64e7bf6431b26adbad5b
Роман Димитриевич <romandimitrievich@MacBook-Air-User.local>
2018-12-12 00:29:31 +0200

    emit new message on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,17 +2,12 @@ let socket = io();
 
 socket.on('connect', function(){
   console.log('Connected to the server');
-
-  socket.emit('createMessage', {
-    from: 'TestClient',
-    text: 'Test answer!'
-  })
 });
 
 socket.on('disconnect', function(){
   console.log('Disconnected from the server');
 });
 
-socket.on('newMessage', function(newMessage){
-  console.log('New message!', newMessage);
+socket.on('newMessage', function(message){
+  console.log('New message!', message);
 });
