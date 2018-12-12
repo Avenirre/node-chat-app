9557ec9c0454036fa30af145d6535c2aa27dd8c3
Роман Димитриевич <romandimitrievich@MacBook-Air-User.local>
2018-12-13 00:47:56 +0200

    scrolling

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,20 @@
-let socket = io();
+var socket = io();
+
+function scrollToBottom () {
+  // Selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child')
+  // Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
 
 socket.on('connect', function () {
   console.log('Connected to server');
@@ -9,33 +25,35 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (message) {
-  let formattedTime = moment(message.createdAt).format('h:mm a');
-  let template = jQuery('#message-template').html();
-  let html = Mustache.render(template, {
+  var formattedTime = moment(message.createdAt).format('h:mm a');
+  var template = jQuery('#message-template').html();
+  var html = Mustache.render(template, {
     text: message.text,
     from: message.from,
     createdAt: formattedTime
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
-  let formattedTime = moment(message.createdAt).format('h:mm a');
-  let template = jQuery('#location-message-template').html();
-  let html = Mustache.render(template, {
+  var formattedTime = moment(message.createdAt).format('h:mm a');
+  var template = jQuery('#location-message-template').html();
+  var html = Mustache.render(template, {
     from: message.from,
     url: message.url,
     createdAt: formattedTime
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
 
-  let messageTextbox = jQuery('[name=message]');
+  var messageTextbox = jQuery('[name=message]');
 
   socket.emit('createMessage', {
     from: 'User',
@@ -45,7 +63,7 @@ jQuery('#message-form').on('submit', function (e) {
   });
 });
 
-let locationButton = jQuery('#send-location');
+var locationButton = jQuery('#send-location');
 locationButton.on('click', function () {
   if (!navigator.geolocation) {
     return alert('Geolocation not supported by your browser.');
